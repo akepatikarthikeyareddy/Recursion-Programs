a5e8173293ce4fa6a6e42deeff3dcb524af41ba5
Akepati Karthikeya Reddy <118301621+akepatikarthikeyareddy@users.noreply.github.com>
2022-12-06 14:56:09 +0530

    Update LeetCodeCombinationSumTwo.java

--- LeetCodeCombinationSumTwo.java
+++ LeetCodeCombinationSumTwo.java
@@ -1,7 +1,7 @@
 /*
 ----------Problem Statement----------
-Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sum to target.
-Each number in candidates may only be used once in the combination.
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates 
+where the candidate numbers sum to target. Each number in candidates may only be used once in the combination.
 Note: The solution set must not contain duplicate combinations.
 
 Input : candidates = [10,1,2,7,6,1,5], target = 8
